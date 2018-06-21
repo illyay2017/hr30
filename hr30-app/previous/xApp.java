56f4689ad901a0839f5ef034267cd8be4b557c48
illyay2017 <illya.yanchuk@gmail.com>
2018-06-21 16:58:11 -0700

    finished

---
+++ hr30-app/previous/xApp.java
@@ -0,0 +1,51 @@
+/*
+package illya.hr30.app;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class App {
+
+  int theNUMBER;
+  int max;
+  Scanner scanner = new Scanner(System.in);
+
+  public App() {
+    Random rand = new Random();
+    max = 100;
+    theNUMBER = Math.abs(rand.nextInt()) % (max + 1);
+  }
+
+  public void play() {
+    while(true) {
+      int move = scanner.nextInt();
+      if (move > theNUMBER) {
+        System.out.println("Your number is too big");
+      } else if (move < theNUMBER) {
+        System.out.println("Your number is too small");
+      } else {
+        System.out.println("You got it, bro!");
+        break;
+      }
+    }
+  }
+  
+  public static void howBigIsMyNumber(int x) {
+    if (x >= 0 && x <= 10) {
+      System.out.println("Our number is pretty small");
+    } else if (x >= 11 && x<= 100) {
+      System.out.println("Our number is pretty big");
+    } else {
+      System.out.println("Our number is out of range");
+    }
+  }
+
+  public static void main (String[] args) {
+    App guessGame = new App();
+    System.out.println("Welcome to my game. Try an guess my impossible"
+      + " number. It's between 0 and " + guessGame.max + " inclusive. "
+      + " Just type a number to get started.");
+    guessGame.play();
+  }
+}
+*/
---
+++ hr30-app/previous/xApp4.java
@@ -0,0 +1,44 @@
+/*
+package illya.hr30.app;
+// to run: mvn package
+// followed up java -cp target/hr30-app-1.0-SNAPSHOT.jar illya.hr30.app.App
+
+public class App {
+
+  public static int Summation(int n) {
+    // Base case:
+    if (n <=0) {
+      return 0; // additive identity property --> 3 + 2 + 1 + 0
+    } else {
+      // Recursive case:
+      return n + Summation(n - 1);
+    }
+  }
+
+  public static int Factorial(int n) {
+    if (n <= 1) {
+      return 1; // multiplicative identity property --> 3 * 2 * 1
+    } else {
+      return n * Factorial(n - 1);
+    }
+  }
+
+  public static int Exponentiation(int n, int m) {
+    if (m == 0) {
+      return 1;
+    } else if (m == 1) {
+      return n;
+    } else {
+      return n * Exponentiation(n, m - 1);
+    }
+  }
+
+  public static void main(String[] args) {
+    System.out.println("The summation from 3 to 0 is: " + Summation(3));
+    System.out.println("6 factorial is " + Factorial(6));
+    System.out.println("2 cubed is " + Exponentiation(2, 3));
+    System.out.println("4 to the 4th " + Exponentiation(4, 4));
+  }
+}
+
+*/
